3609584299ac8001d54e0d3ce665cb6213f44377
NeuroRulez <neuro_999@hotmail.com>
2023-09-06 14:21:06 +0200

    Fix

--- neogeo.sv
+++ neogeo.sv
@@ -196,7 +196,7 @@ module emu
 assign ADC_BUS  = 'Z;
 
 wire         CLK_JOY = CLK_50M;         //Assign clock between 40-50Mhz
-wire   [2:0] JOY_FLAG  = {status[30],status[31],status[29]}; //Assign 3 bits of status (31:29) o (63:61)
+wire   [2:0] JOY_FLAG  = {status[62],status[63],status[61]}; //Assign 3 bits of status (31:29) o (63:61)
 wire         JOY_CLK, JOY_LOAD, JOY_SPLIT, JOY_MDSEL;
 wire   [5:0] JOY_MDIN  = JOY_FLAG[2] ? {USER_IN[6],USER_IN[3],USER_IN[5],USER_IN[7],USER_IN[1],USER_IN[2]} : '1;
 wire         JOY_DATA  = JOY_FLAG[1] ? USER_IN[5] : '1;
@@ -281,9 +281,9 @@ localparam CONF_STR = {
 	"FS1,*,Load ROM set;",
 	"S1,CUECHD,Load CD Image;",
 "-;",
-	"OUV,UserIO Joystick,Off,DB9MD,DB15 ;",
-	"OT,UserIO Players, 1 Player,2 Players;",
-	"OS,Buttons Config.,ABCX(ABCD),ABCZ;",
+	"oUV,UserIO Joystick,Off,DB9MD,DB15 ;",
+	"oT,UserIO Players, 1 Player,2 Players;",
+	"oS,Buttons Config.,ABCX(ABCD),ABCZ;",
 "-;",
 	"H3OP,FM,ON,OFF;",
 	"H3OQ,ADPCMA,ON,OFF;",
@@ -496,7 +496,7 @@ wire [15:0] sdram_sz;
 wire [21:0] gamma_bus;
 
 wire [31:0] joystick_0 = joydb_1ena ?
-	!status[28] ? {
+	!status[60] ? {
 		// Z M Y S X C B A U D L R
 		OSD_STATUS? 32'b000000 : {joydb_1[9],joydb_1[11]|(joydb_1[10]&joydb_1[5]),joydb_1[8],joydb_1[10],joydb_1[7],joydb_1[6:0]}
 		} :
@@ -507,7 +507,7 @@ wire [31:0] joystick_0 = joydb_1ena ?
 : joystick_0_USB;
 
 wire [31:0] joystick_1 = joydb_2ena ?
-	!status[28] ? {
+	!status[60] ? {
 		// Z M Y S X C B A U D L R
 		OSD_STATUS? 32'b000000 : {joydb_2[9],joydb_2[11]|(joydb_2[10]&joydb_2[5]),joydb_2[8],joydb_2[10],joydb_2[7],joydb_2[6:0]}
 		} :
